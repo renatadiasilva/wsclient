d939af3db91b6187fcccffb932429ba4c770909e
renatadiasilva <renatadiasilva@gmail.com>
2015-06-26 11:08:39 +0100

    updateUser implementado

--- src/main/java/dei/uc/pt/aor/WSConsumer.java
+++ src/main/java/dei/uc/pt/aor/WSConsumer.java
@@ -26,10 +26,11 @@ public class WSConsumer {
 			System.out.println("\n\n***** Choose *****");
 			System.out.println("1 - List users ");
 			System.out.println("2 - See user (by email)");
-			System.out.println("3 - Edit user (by email)");
+			System.out.println("3 - Edit user name (by email)");
 			System.out.println("4 - Create new user");
 			System.out.println("5 - Delete user (by email)");
 			System.out.println("6 - Quit");
+			System.out.println("7 - Edit user pass (by email)");
 
 			try {
 				answer = reader.readLine();
@@ -39,6 +40,8 @@ public class WSConsumer {
 				stop = true;
 				answer = "6";
 			}
+			
+			System.out.println("\n");
 
 			int op = 1000;
 			try {
@@ -49,6 +52,7 @@ public class WSConsumer {
 			// try catch
 			switch (op) {
 			case 1: 
+				System.out.println("Listing all users...\n\n");
 				listAllUsers();
 				break;
 			case 2: 
@@ -65,7 +69,19 @@ public class WSConsumer {
 				}
 				break;
 			case 3: 
-				System.out.println("Not implemented yet");
+				try {
+					System.out.println("Insert the email: ");
+					email = reader.readLine();
+					//validacoes
+					System.out.println("Insert new user name: ");
+					String name = reader.readLine();
+					updateUser(email, name, "");
+				} catch (IOException e) {
+					// usar log
+					e.printStackTrace();
+					stop = true;
+					answer = "6";
+				}
 				break;
 			case 4: 
 				try {
@@ -96,6 +112,22 @@ public class WSConsumer {
 				}
 				break;
 			case 6: stop = true; break;
+			case 7: 
+				try {
+					System.out.println("Insert the email: ");
+					email = reader.readLine();
+					//validacoes
+					System.out.println("Insert the new pass: ");
+					String pass = reader.readLine();
+					// confirm pass??
+					updateUser(email, "", pass);
+				} catch (IOException e) {
+					// usar log
+					e.printStackTrace();
+					stop = true;
+					answer = "6";
+				}
+				break;
 			default: stop = false;
 			}
 
@@ -103,55 +135,6 @@ public class WSConsumer {
 
 	}
 
-	private static void createUser(String name, String email) {
-
-		ResteasyClient client = new ResteasyClientBuilder().build();
-
-		ResteasyWebTarget target = client.target("http://localhost:8080/playlist-wsclient/rest/users/createuser");
-
-		target = target.queryParam("name", name);
-		target = target.queryParam("email", email);
-
-//		System.out.println(target.getUri());
-
-		User theuser = new User();
-
-		// request e response já terão de ser criados por request e response
-		Response response = target.request().post(Entity.entity(theuser, "application/xml"));
-
-		if (response.getStatus() != 200) { // se nao correu tudo bem !
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ response.getStatus());
-			// já existe email?
-//			org.postgresql.util.PSQLException
-		} else System.out.println(response.readEntity(User.class));
-
-
-	}
-
-	private static void deleteUser(String email) {
-
-		ResteasyClient client = new ResteasyClientBuilder().build();
-
-		ResteasyWebTarget target = client.target("http://localhost:8080/playlist-wsclient/rest/users/deleteuser");
-
-		target = target.path(""+email);
-
-		System.out.println(target.getUri());
-
-		Response response = target.request().delete();
-
-		if (response.getStatus() != 200) { // se nao correu tudo bem !
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ response.getStatus());
-			// não existe email?
-//			org.postgresql.util.PSQLException
-		} else System.out.println("Deleted user with email "+email); // qq coisa com response?
-
-
-	}
-
-	
 	private static void listAllUsers() {
 		ResteasyClient client = new ResteasyClientBuilder().build();
 
@@ -185,5 +168,81 @@ public class WSConsumer {
 //		}
 
 	}
+
+	//esta a criar user com pass 123 por defeito
+	private static void createUser(String name, String email) {
+
+		ResteasyClient client = new ResteasyClientBuilder().build();
+
+		ResteasyWebTarget target = client.target("http://localhost:8080/playlist-wsclient/rest/users/createuser");
+
+		target = target.queryParam("name", name);
+		target = target.queryParam("email", email);
+
+//		System.out.println(target.getUri());
+
+		User user = new User();
+
+		// request e response já terão de ser criados por request e response
+		Response response = target.request().post(Entity.entity(user, "application/xml"));
+
+		if (response.getStatus() != 200) { // se nao correu tudo bem !
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus());
+			// já existe email?
+//			org.postgresql.util.PSQLException
+		} else System.out.println(response.readEntity(User.class));
+
+
+	}
+
+	private static void deleteUser(String email) {
+
+		ResteasyClient client = new ResteasyClientBuilder().build();
+
+		ResteasyWebTarget target = client.target("http://localhost:8080/playlist-wsclient/rest/users/deleteuser");
+
+		target = target.path(""+email);
+
+		System.out.println(target.getUri());
+
+		Response response = target.request().delete();
+
+		if (response.getStatus() != 200) { // se nao correu tudo bem !
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus());
+			// não existe email?
+//			org.postgresql.util.PSQLException
+		} else System.out.println("Deleted user with email "+email); // qq coisa com response?
+
+
+	}
+	
+	private static void updateUser(String email, String name, String pass) {
+
+		ResteasyClient client = new ResteasyClientBuilder().build();
+
+		ResteasyWebTarget target = client.target("http://localhost:8080/playlist-wsclient/rest/users/updateuser");
+
+		target = target.path(""+email);  // ver se existe!!
+		
+		if (!name.equals("")) target = target.queryParam("name", name);
+		if (!pass.equals("")) target = target.queryParam("pass", pass);
+
+//		System.out.println(target.getUri());
+
+		User user = new User();
+		Response response = target.request().put(Entity.entity(user, "application/xml"));
+
+		if (response.getStatus() != 200) { // se nao correu tudo bem !
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus());
+			// não existe email?
+//			org.postgresql.util.PSQLException
+		} else System.out.println("Updated user with email "+email); // qq coisa com response?
+
+
+	}
+	
 
 }
