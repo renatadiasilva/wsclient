eaf58577aebbf0a280c970b3b7dc89b5e143ebf0
renatadiasilva <renatadiasilva@gmail.com>
2015-06-28 16:09:00 +0100

    tostring dos collections

--- src/main/java/dei/uc/pt/aor/PlaylistCollection.java
+++ src/main/java/dei/uc/pt/aor/PlaylistCollection.java
@@ -22,12 +22,12 @@ public class PlaylistCollection {
 	public String toString() {
 		String s = "";
 		if ( (listPlaylists == null) || (listPlaylists.size() == 0) ) return "No playlists";
-		String su = String.format("%4s %13s %28s\n", "ID", "NAME", "DATE OF CREATION", "SIZE", "OWNER");
+		String su = String.format("%4s %13s %28s %20s \n", "ID", "NAME", "SIZE", "OWNER");
 		s += su;
 		s += "-------------------------------------------------------------\n";
 		for (Playlist p: listPlaylists) {
-			su = String.format("%4d  %-25s %-30s \n", p.getId(), p.getName(),
-					p.getDateOfCriation(), p.getSize(), p.getOwner());
+			su = String.format("%4d  %-25s %10d %-30s \n", p.getId(), p.getName(),
+					p.getSize(), p.getOwner().getEmail());
 			s += su;
 		}
 		return  s;
--- src/main/java/dei/uc/pt/aor/SongCollection.java
+++ src/main/java/dei/uc/pt/aor/SongCollection.java
@@ -1,5 +1,36 @@
 package dei.uc.pt.aor;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="collection")
 public class SongCollection {
+	
+	@XmlElement(name="song")
+	private List<Song> listSongs;
+	
+	public List<Song> getList() {
+		return listSongs;
+	}
+
+	public void setList(List<Song> list) {
+		this.listSongs = list;
+	}
+	
+	public String toString() {
+		String s = "";
+		if ( (listSongs == null) || (listSongs.size() == 0) ) return "No songs";
+		String su = String.format("%4s %13s %28s %20s \n", "ID", "NAME", "SIZE", "OWNER");
+		s += su;
+		s += "-------------------------------------------------------------\n";
+		for (Song ss: listSongs) {
+			su = String.format("%4d %-20s %-20s %-20s %10d %-30s \n", ss.getId(), ss.getTitle(),
+					ss.getArtist(), ss.getAlbum(), ss.getReleaseYear(), ss.getOwner().getEmail());
+			s += su;
+		}
+		return  s;
+	}
 
 }
